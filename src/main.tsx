c385ba06a994a5538b0396f343ddbb8f56a15093
dxzfvzs <kristynafuchsova@seznam.cz>
2026-08-04 01:03:04 +0200

    feat: css restructure and colors tucked away into their files

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import App from './App'
 import './styles/global.css'
+import './styles/colors.css'
 import './styles/text.css'
-import './styles/themes/light.css'
-import './assets/data/category-colors.css'
+import './styles/category-colors.css'
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById('root')!).render(
