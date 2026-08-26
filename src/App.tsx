62545aadf6e1c1b4b9e60d2116457a12c17f27f5
dxzfvzs <kristynafuchsova@seznam.cz>
2026-08-26 23:32:31 +0200

    feat: swap to hash router for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Layout from "./Layout.tsx";
 import AllRecipes from "./pages/AllRecipes.tsx";
 import RecipePage from "./pages/RecipePage.tsx";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <HashRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<AllRecipes/>}/>
-          <Route path="/recipe/:id" element={<RecipePage />} />
+          <Route path="/recipe/:id" element={<RecipePage/>}/>
 
           <Route path="*" element={<Navigate to="/" replace/>}/>
         </Routes>
       </Layout>
-    </Router>
+    </HashRouter>
   );
 };
 
